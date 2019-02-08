e4e66cb1eb2b8822a6e98ff2239bc39bbdd9d2e2
Toshiya Doi <doi.t.npdp@gmail.com>
2019-02-08 17:50:29 +0900

    upgrade terraform version

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  version = "~> 1.39.0"
+  version = "~> 1.56.0"
   region  = "ap-northeast-1"
 }
 
 terraform {
-  required_version = "= 0.11.8"
+  required_version = "= 0.11.11"
 
   backend "s3" {}
 }
